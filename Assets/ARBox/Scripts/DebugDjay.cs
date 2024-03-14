b699b99b75669a617144b8e78f4424a1aad8108e
Jay484 <djhinkwan09@gmail.com>
2024-03-14 19:30:08 +0530

    eyeview adjustable

--- Assets/ARBox/Scripts/DebugDjay.cs
+++ Assets/ARBox/Scripts/DebugDjay.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 public class DebugDjay
 {
-    public static void Log(string message)
+    public static void Log(object message)
     {
         Debug.Log("testJay: " + message);
     }
 
-    public static void Error(string message)
+    public static void Error(object message)
     {
         Debug.LogError("testJay: "+message);
     }
